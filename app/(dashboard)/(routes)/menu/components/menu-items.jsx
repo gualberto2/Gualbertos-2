4e80da67963aa390070b016a91b4c6d762df96b6
gualberto2 <elijahwilsonnn@gmail.com>
2023-10-26 11:27:18 -0700

    making it work

---
+++ app/(dashboard)/(routes)/menu/components/menu-items.jsx
@@ -0,0 +1,374 @@
+export const comboPlates = [
+  { id: "combo-1", title: "#1. Two Beef or Chicken Tacos", price: "" },
+  { id: "combo-2", title: "#2. Two Enchiladas", price: "" },
+  { id: "combo-3", title: "#3. Beef Taco & Beef Burrito", price: "" },
+  { id: "combo-4", title: "#4. Beef Taco & Cheese Enchilada", price: "" },
+  { id: "combo-5", title: "#5. Beef Burrito & Cheese Enchilada", price: "" },
+  { id: "combo-6", title: "#6. Cheese Enchilada & Beef Tostada", price: "" },
+  { id: "combo-7", title: "#7. Carne Asada Plate", price: "" },
+  { id: "combo-8", title: "#8. Chorizo or Machaca Plate", price: "" },
+  { id: "combo-9", title: "#9. Two Beef or Chicken Burritos", price: "" },
+  { id: "combo-10", title: "#10. Two Chile Relleno Plate", price: "" },
+  { id: "combo-11", title: "#11. Two Carne Asada Tacos", price: "" },
+  { id: "combo-12", title: "#12. Chimichanga W/ Rice and Beans", price: "" },
+  { id: "combo-13", title: "#13. Carnitas Plate", price: "" },
+  { id: "combo-14", title: "#14. Cheese Enchilada", price: "" },
+  { id: "combo-15", title: "#15. Tamale & Beef Taco", price: "" },
+  { id: "combo-16", title: "#16. Chile Verde Plate (Pork)", price: "" },
+  { id: "combo-17", title: "#17. Pollo en Salsa Chipotle", price: "" },
+  { id: "combo-18", title: "#18. 2 Fish Tacos Plate", price: "" },
+  { id: "combo-19", title: "#19. Steak Milanesa", price: "" },
+  { id: "combo-20", title: "#20. 2 Beef or Chicken Flautas", price: "" },
+  { id: "combo-21", title: "#21. Steak Ranchero", price: "" },
+  { id: "combo-22", title: "#22. Shrimp Enchiladas", price: "" },
+  {
+    id: "combo-23",
+    title: "#23. Fajitas (Pollo Asado or Carne Asada)",
+    price: "",
+  },
+  {
+    id: "combo-24",
+    title: "#24. Surf & Turf (Shrimp or Carne Asada)",
+    price: "",
+  },
+  { id: "combo-25", title: "#25. Pechuga Empanizada", price: "" },
+];
+
+export const breakfast = [
+  {
+    id: "breakfast-1",
+    title: "#1. Two Eggs, Potatoes, Beans, Ham & Toast",
+    price: "",
+  },
+  { id: "breakfast-2", title: "#2. Three Pancakes", price: "" },
+  { id: "breakfast-3", title: "#3. Breakfast Plate", price: "" },
+  { id: "breakfast-4", title: "#4. Ham Omelette", price: "" },
+  { id: "breakfast-5", title: "#5. Chorizo W/ Eggs, Rice & Beans", price: "" },
+  { id: "breakfast-6", title: "#6. Machaca W/Eggs, Rice & Beans", price: "" },
+  { id: "breakfast-7", title: "#7. Ham W/Eggs, Rice & Beans", price: "" },
+  {
+    id: "breakfast-8",
+    title: "#8. Huevos Rancheros W/ Rice & Beans",
+    price: "",
+  },
+  { id: "breakfast-9", title: "#9. French Toast W/ Bacon & Eggs", price: "" },
+  {
+    id: "breakfast-10",
+    title: "#10. Huevos a la Mexicana W/ Rice & Beans",
+    price: "",
+  },
+  { id: "breakfast-11", title: "#11. Breakfast Sandwhich", price: "" },
+  { id: "breakfast-12", title: "#12. Chilaquiles", price: "" },
+  { id: "breakfast-13", title: "#13. Chilaquiles W/ Eggs", price: "" },
+  { id: "breakfast-14", title: "#14. Shrimp Omelettes", price: "" },
+  {
+    id: "breakfast-15",
+    title: "#15. Sausage W/ Eggs, Rice and Beans",
+    price: "",
+  },
+  { id: "breakfast-16", title: "#16. Carneasada Omelette", price: "" },
+];
+
+export const breakfastBurritos = [
+  {
+    id: "burrito-1",
+    title: "#1. Eggs, Bacon, Cheese, Rice & Beans Burrito",
+    price: "",
+  },
+  {
+    id: "burrito-2",
+    title: "#2. Eggs, Cheese, Potatoes Burrito (Choose 1: Ham, Bacon, Sausage)",
+    price: "",
+  },
+  { id: "burrito-3", title: "#3. Eggs, Cheese, Potatoes Burrito", price: "" },
+  {
+    id: "burrito-4",
+    title: "#4. Carneasada, Potatoes, Egg & Cheese Burrito",
+    price: "",
+  },
+  {
+    id: "burrito-5",
+    title: "#5. Chorizo W/ Potatoes & Egg Burrito",
+    price: "",
+  },
+];
+
+export const enchiladas = [
+  { id: "enchilada-1", title: "Two Cheese Enchiladas", price: "" },
+  { id: "enchilada-2", title: "Two Beef Enchiladas", price: "" },
+  { id: "enchilada-3", title: "2 Chicken Enchiladas", price: "" },
+  { id: "enchilada-4", title: "Shrimp Enchiladas", price: "" },
+  { id: "enchilada-5", title: "Carne Asada Enchiladas", price: "" },
+];
+
+export const tostadas = [
+  { id: "tostada-1", title: "#1. Flying Saucer (Chicken or Beef)", price: "" },
+  {
+    id: "tostada-2",
+    title: "#2. Flying Saucer (Carneasada or Carnita)",
+    price: "",
+  },
+  {
+    id: "tostada-3",
+    title: "#3. Supreme Tostada (Chicken or beef)",
+    price: "",
+  },
+  { id: "tostada-4", title: "#4. Supreme Tostada (Carneasada)", price: "" },
+  { id: "tostada-5", title: "#5. Bean Tostada", price: "" },
+  { id: "tostada-6", title: "#6. Beef Tostada", price: "" },
+  { id: "tostada-7", title: "#7. Chicken Tostada", price: "" },
+  { id: "tostada-8", title: "#8. Carneasada Tostada", price: "" },
+  { id: "tostada-9", title: "#9. Carnitas Tostada", price: "" },
+  { id: "tostada-10", title: "#10. Ceviche Tostada", price: "" },
+];
+
+export const gualbertosSpecialty = [
+  {
+    id: "specialty-1",
+    title: "2 Flautas",
+    price: "",
+  },
+  {
+    id: "specialty-2",
+    title: "Taco Plate W/ Rice & Beans",
+    price: "",
+  },
+  {
+    id: "specialty-3",
+    title: "3 Rolled tacos W/ Rice & Beans",
+    price: "",
+  },
+  {
+    id: "specialty-4",
+    title: "Burrito Plate",
+    price: "",
+  },
+  {
+    id: "specialty-5",
+    title: "5 Roll Tacos",
+    price: "",
+  },
+  {
+    id: "specialty-6",
+    title: "12 Roll tacos",
+    price: "",
+  },
+  {
+    id: "specialty-7",
+    title: "Big plate",
+    price: "",
+  },
+];
+
+export const burritos = [
+  { id: "burrito-1", title: "Pastor (Pork Adobado)", price: "" },
+  { id: "burrito-2", title: "Pollo Asado (Grilled Chicken)", price: "" },
+  { id: "burrito-3", title: "Fish", price: "" },
+  { id: "burrito-4", title: "Bean and Cheese", price: "" },
+  { id: "burrito-5", title: "Beef", price: "" },
+  { id: "burrito-6", title: "Mix", price: "" },
+  { id: "burrito-7", title: "Chicken", price: "" },
+  { id: "burrito-8", title: "Chorizo", price: "" },
+  { id: "burrito-9", title: "Machaca", price: "" },
+  { id: "burrito-10", title: "Carneasada", price: "" },
+  { id: "burrito-11", title: "Chile Relleno", price: "" },
+  { id: "burrito-12", title: "Carnitas", price: "" },
+  { id: "burrito-13", title: "Ranchero", price: "" },
+  { id: "burrito-14", title: "Shrimp", price: "" },
+  { id: "burrito-15", title: "California", price: "" },
+  { id: "burrito-16", title: "Chile Verde", price: "" },
+  { id: "burrito-17", title: "Vegetarian", price: "" },
+  { id: "burrito-18", title: "Pollo Adobado", price: "" },
+];
+
+export const soups = [
+  { id: "soup-1", title: "Caldo De Pollo", price: "" },
+  { id: "soup-2", title: "Caldo De Res", price: "" },
+  { id: "soup-3", title: "Albondigas", price: "" },
+  { id: "soup-4", title: "Pozole", price: "" },
+  { id: "soup-5", title: "Tlalpeno", price: "" },
+  { id: "soup-6", title: "Menudo", price: "" },
+];
+
+export const seafood = [
+  { id: "fish-1", title: "Shrimp Rancheros", price: "" },
+  { id: "fish-2", title: "Shrimp in Garlic Sauce", price: "" },
+  { id: "fish-3", title: "Breaded Shrimp", price: "" },
+  { id: "fish-4", title: "Shrimps a la Diabla", price: "" },
+  { id: "fish-5", title: "Shrimp Cocktail", price: "" },
+  { id: "fish-6", title: "Caldo De Camaron", price: "" },
+  { id: "fish-7", title: "Caldo De 7 Mares (7 Seas)", price: "" },
+  { id: "fish-8", title: "Caldo De Pescado", price: "" },
+  { id: "fish-9", title: "Campechana", price: "" },
+  { id: "fish-10", title: "Fish Fillet", price: "" },
+];
+
+export const chimichangas = [
+  { id: "chimichanga-1", title: "Chicken Chimichanga", price: "" },
+  { id: "chimichanga-2", title: "Beef chimichanga", price: "" },
+  { id: "chimichanga-3", title: "Carneasada Chimichanga", price: "" },
+  { id: "chimichanga-4", title: "Carnitas Chimichanga", price: "" },
+];
+
+export const carneasadaFries = [
+  { title: "Carneasada fries", price: "" },
+  { title: "Half Carne Fries", price: "" },
+  { title: "Carnitas Fries", price: "" },
+  { title: "Pollo Asado fries", price: "" },
+  { title: "Adobada Fries", price: "" },
+  { title: "Half Carnitas Fries", price: "" },
+  { title: "Half Adobada Fries", price: "" },
+  { title: "Half Pollo Asado Fries", price: "" },
+];
+
+export const surfAndTurfFries = [
+  { title: "Surf & Turf Fries", price: "" },
+  { title: "Half Surf & Turf Fries", price: "" },
+  { title: "Campechana Fries", price: "" },
+];
+
+export const tortas = [
+  { title: "Beef Torta", price: "" },
+  { title: "Ham Torta", price: "" },
+  { title: "Chicken Torta", price: "" },
+  { title: "Machaca Torta", price: "" },
+  { title: "Carneasada Torta", price: "" },
+  { title: "Carnitas Torta", price: "" },
+  { title: "Chorizo Torta", price: "" },
+  { title: "Milanesa Torta", price: "" },
+  { title: "Pastor Torta", price: "" },
+  { title: "Pollo Asado Torta", price: "" },
+  { title: "Milanesa Torta + Ham & Cheese", price: "" },
+];
+
+export const chips = [
+  { title: "Carne Asada Chips", price: "" },
+  { title: "Carnitas Chips", price: "" },
+  { title: "Pastor Chips", price: "" },
+  { title: "Pollo Asado Chips", price: "" },
+  { title: "Veggie Chips", price: "" },
+  { title: "Chips W/ Cheese", price: "" },
+  { title: "Chips W/ Guac", price: "" },
+  { title: "Nachos", price: "" },
+  { title: "Half Carne Asada Chips", price: "" },
+  { title: "Half Carnitas Chips", price: "" },
+  { title: "Half Pollo Asado Chips", price: "" },
+  { title: "Half Adobada Chips", price: "" },
+  { title: "Surf & Turf Chips", price: "" },
+  { title: "Half Surf & Turf Chips", price: "" },
+  { title: "Chips & Salsa", price: "" },
+];
+
+export const sides = [
+  { title: "Small Rice (8oz)", price: "" },
+  { title: "Regular Rice (20oz)", price: "" },
+  { title: "Large Rice (32oz)", price: "" },
+  { title: "Small Beans (8oz)", price: "" },
+  { title: "Regular Beans (20oz)", price: "" },
+  { title: "Large Rice (32oz)", price: "" },
+];
+
+export const quesadillas = [
+  { title: "Camaron Quesadilla", price: "" },
+  { title: "Carnitas Quesadilla", price: "" },
+  { title: "Carneasada Quesadilla", price: "" },
+  { title: "Pollo Asado Quesadilla", price: "" },
+  { title: "Pastor Quesadilla", price: "" },
+  { title: "Beef Quesadilla", price: "" },
+  { title: "Chicken Quesadilla", price: "" },
+  { title: "Ham Quesadilla", price: "" },
+  { title: "Chicken Chipotle Quesadilla", price: "" },
+  { title: "Plain Quesadilla", price: "" },
+];
+
+export const superQuesadillas = [
+  { title: "Carnitas Super Quesadilla", price: "" },
+  { title: "Carneasada Super Quesadilla", price: "" },
+  { title: "Pollo Asado Super Quesadilla", price: "" },
+  { title: "Pastor Super Quesadilla", price: "" },
+  { title: "Beef Super Quesadilla", price: "" },
+  { title: "Chicken Super Quesadilla", price: "" },
+  { title: "Ham Super Quesadilla", price: "" },
+  { title: "Chicken Chiptole Super Quesadilla", price: "" },
+  { title: "Camaron Super Quesadilla", price: "" },
+  { title: "Mary Tierra Super Quesadilla", price: "" },
+];
+
+export const tacos = [
+  { title: "Carneasada Taco", price: "" },
+  { title: "Carnitas Taco", price: "" },
+  { title: "Pollo Asado taco", price: "" },
+  { title: "Fish Taco", price: "" },
+  { title: "Shrimp Taco", price: "" },
+  { title: "Pastor Taco", price: "" },
+  { title: "3 Roll Tacos (Cheese only)", price: "" },
+  { title: "3 Roll Tacos W/ Guac", price: "" },
+  { title: "Chicken Taco", price: "" },
+  { title: "Beef Taco", price: "" },
+  { title: "Mulita", price: "" },
+  { title: "Beef or Chicken or Potato Taco", price: "" },
+];
+
+export const extra = [
+  { title: "French Fries", price: "" },
+  { title: "Tortilla", price: "" },
+  { title: "Beans", price: "" },
+  { title: "Guacamole", price: "" },
+  { title: "Rice", price: "" },
+  { title: "Sour Cream", price: "" },
+  { title: "Cheese", price: "" },
+  { title: "Egg", price: "" },
+  { title: "Bunuelos", price: "" },
+  { title: "Ham", price: "" },
+  { title: "Bacon", price: "" },
+  { title: "Sausage", price: "" },
+  { title: "Tamale", price: "" },
+  { title: "8oz Sour Cream", price: "" },
+  { title: "Box of Chips & Salsa", price: "" },
+  { title: "Carneasada", price: "" },
+  { title: "Shrimp", price: "" },
+  { title: "8oz Guac", price: "" },
+  { title: "Any Enchilada", price: "" },
+  { title: "Chile Relleno", price: "" },
+];
+
+export const hamburgers = [
+  { title: "Jumbo Double Meat & Cheese", price: "" },
+  { title: "Cheeseburger", price: "" },
+  { title: "Cheeseburger W/ Fries", price: "" },
+  { title: "Hamburger", price: "" },
+  { title: "French Fries", price: "" },
+];
+
+export const salads = [
+  { title: "Shrimp Salad", price: "" },
+  { title: "Chicken Salad", price: "" },
+  { title: "Pollo Asado Salad", price: "" },
+  { title: "Carneasada Salad", price: "" },
+  { title: "Green Salad", price: "" },
+  { title: "Surf & Turf Salad", price: "" },
+];
+
+export const partyPacks = [
+  { title: "Chips & Salsa (20 People)", price: "" },
+  { title: "Quesadilla Tray", price: "" },
+  { title: "Rolled Tacos (Platter of 50)", price: "" },
+  { title: "Enchilada (Platter of 20)", price: "" },
+  { title: "Make Your Own Tacos", price: "" },
+  { title: "Side Orders (Serves 20)", price: "" },
+];
+
+export const drinks = [
+  { title: "Soda Can", price: "" },
+  { title: "Coke Glass Bottles", price: "" },
+  { title: "Aguas Frescas (Medium)", price: "" },
+  { title: "Aguas Frescas (Large)", price: "" },
+  { title: "Coffee", price: "" },
+  { title: "Water Bottle", price: "" },
+  { title: "Milk", price: "" },
+];
+
+export const kidsMenu = [
+  { title: "Kids Quesadilla Combo", price: "" },
+  { title: "Kids Bean & Cheese Burrito", price: "" },
+];
