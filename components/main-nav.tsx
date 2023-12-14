995d1b1e48016f500bd9a566372f9eca44e3b4af
gualberto2 <elijahwilsonnn@gmail.com>
2023-12-14 07:50:16 -0800

    order online redirects to food ordering site

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -27,7 +27,7 @@ export function MainNav({ className }: React.HTMLAttributes<HTMLElement>) {
       active: pathname === "/contact",
     },
     {
-      href: "/order",
+      href: "https://food.google.com/chooseprovider?restaurantId=/g/1tfm6z14&g2lbs=ANTchaMpSH41hd2FOcGGPQWAT02MnF0CoeDtVHNHA0cDEELgkkopsL9-83bq6Tsmtht8HXCbqkJ-ZAl2UKZqPkjdJbfuYyMn6du2vwVVBy8nMal3OxQK16hTFH14fRHCnYwj0fkpYPRD5LzlzG77G5kxH1iFgsnCHA%3D%3D&hl=en-US&gl=us&cs=1&ssta=1&fo_m=MfohQo559jFvMUOzJVpjPL1YMfZ3bInYwBDuMfaXTPp5KXh-&gei=qHx4ZbXKBIH4kPIPqrCVmAI&ei=qHx4ZbXKBIH4kPIPqrCVmAI&fo_s=OA&opi=89978449&sei=CVEf821a8GuyEa1C5IlnHWcx&utm_campaign&utm_source=search",
       label: "Order Online",
       active: pathname === "/order",
     },
