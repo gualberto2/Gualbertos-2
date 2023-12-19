ec5cf2176ca561b0f8f7c15f0017cd06429784dd
gualberto2 <elijahwilsonnn@gmail.com>
2023-12-19 07:27:44 -0800

    made a dropdown component

--- app/(dashboard)/(routes)/menu/components/catering-button.jsx
+++ app/(dashboard)/(routes)/menu/components/catering-button.jsx
@@ -9,24 +9,37 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import Link from "next/link";
 
-const CateringMenu = () => {
+const CateringMenu = ({ className }) => {
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <Button
-          variant="outline"
-          className="border-green-800 text-green-800 hover:bg-green-100"
-        >
-          Catering
-        </Button>
+    <DropdownMenu className="">
+      <DropdownMenuTrigger className="border px-3 rounded-md border-green-800 text-green-800 hover:bg-green-100">
+        Catering
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuLabel>Contact</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>Phone</DropdownMenuItem>
-        <DropdownMenuItem>Email</DropdownMenuItem>
-        <DropdownMenuItem>Text</DropdownMenuItem>
+        <DropdownMenuItem>
+          <div className="flex flex-row justify-between gap-2">
+            <h4>Call:</h4>
+            <Link href="tel:+18582683209" className="">
+              (858) 268-3209
+            </Link>
+          </div>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <div className="flex flex-row justify-between gap-2">
+            <h4>Email:</h4>
+            <p>juans@gualbertos2.com</p>
+          </div>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <div className="flex flex-row justify-between gap-2">
+            <h4>Text: </h4>
+            <p>(858) 268-3209</p>
+          </div>
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
