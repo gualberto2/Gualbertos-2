21d59fc5b9f1e6e0b74366e3c78779c83bf1c328
gualberto2 <elijahwilsonnn@gmail.com>
2023-12-20 06:48:53 -0800

    added links to all redirects

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Separator } from "./ui/separator";
@@ -8,39 +9,61 @@ const footer = () => {
       <Separator />
       <div className="cursor-pointer text-sm flex flex-row whitespace-nowrap px-8 mt-8 space-x-2 justify-center tracking-wider">
         {/* ehader tabs */}
-        <p className="underline text-green-900">Visit</p>
-        <p className="underline text-green-900">Menu</p>
-        <p className="underline text-green-900">Story</p>
-        <p className="underline text-green-900">Join Our Team</p>
-        <p className="underline text-green-900">Contact</p>
+        <p className="underline text-green-900">
+          <Link
+            href="https://www.google.com/maps/dir/?api=1&destination=5447+Kearny+Villa+Rd,+San+Diego,+CA+92123"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Visit
+          </Link>
+        </p>
+        <p className="underline text-green-900">
+          <Link href="/menu">Menu</Link>
+        </p>
+        <p className="underline text-green-900">
+          <Link href="/about">Story</Link>
+        </p>
+        <p className="underline text-green-900">
+          <Link href="/apply">Join Our Team</Link>
+        </p>
+        <p className="underline text-green-900">
+          <Link href="/contact">Contact</Link>
+        </p>
       </div>
       <div className="max-w-6xl mx-auto mt-8 ">
         <div className="content-center flex justify-center items-center">
           {/* Logo here */}
           <p className="font-light text-xs  text-center cursor-pointer hover:text-gray-500 transition duration-150 ease-in-out">
-            5447 Kearny Villa Rd, San Diego, CA 92123
+            <Link
+              href="https://www.google.com/maps/dir/?api=1&destination=5447+Kearny+Villa+Rd,+San+Diego,+CA+92123"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              5447 Kearny Villa Rd, San Diego, CA 92123
+            </Link>
           </p>
         </div>
       </div>
       <div className="cursor-pointer text-sm flex flex-row whitespace-nowrap px-8 mt-8 pb-12 space-x-2 justify-center tracking-tight">
         {/* social medias */}
-        <p className="underline text-green-900">Instagram</p>
-        <p className="underline text-green-900">Facebook</p>
-        <p className="underline text-green-900">RestarauntJi</p>
+        <p className="underline text-green-900">
+          <Link href="https://www.instagram.com/gualbertos2tacoshop/">
+            Instagram
+          </Link>
+        </p>
+        <p className="underline text-green-900">
+          <Link href="https://www.facebook.com/profile.php?id=100063684715593">
+            Facebook
+          </Link>
+        </p>
+        <p className="underline text-green-900">
+          <Link href="https://www.restaurantji.com/ca/san-diego/gualbertos-2-/">
+            RestarauntJi
+          </Link>
+        </p>
       </div>
     </footer>
   );
 };
 export default footer;
-
-// https://t.me/RelativeMoveMega
-
-// https://justpaste.it/2kfpr
-
-// https://www.reddit.com/r/milanmanfredihoty/?rdt=38452
-
-// https://spankbang.com/8x64c/video/new+hot+onlyfans+leak+all+content+in+description
-
-// /https://spankbang.com/8xbrz/video/milf
-
-// https://spankbang.com/8xc6m/video/latina+with+a+big+ass
